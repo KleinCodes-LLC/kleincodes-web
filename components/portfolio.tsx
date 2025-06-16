a6788790c166cee8b3f062f1108e866439d3fc64
Michael Klein <kleincodes@gmail.com>
2025-06-16 12:06:44 -0400

    Resolved warnings.

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Portfolio: React.FC = () => {
   return (
@@ -7,9 +8,11 @@ const Portfolio: React.FC = () => {
         {/* Profile Image */}
         <div className="mb-8">
           <div className="w-48 h-48 mx-auto rounded-full overflow-hidden border-4 border-slate-600 shadow-2xl">
-            <img 
+            <Image 
               src="/images/portrait.png" 
               alt="Portrait"
+              width={192}
+              height={192}
               className="w-full h-full object-cover"
             />
           </div>
@@ -40,10 +43,10 @@ const Portfolio: React.FC = () => {
         {/* Company Links */}
         <div className="flex justify-center space-x-4 mb-12">
           <a href="https://www.canary.is" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/canary.png" alt="Canary App Icon" className="w-12 h-12 rounded-lg" />
+            <Image src="/images/canary.png" alt="Canary App Icon" width={48} height={48} className="w-12 h-12 rounded-lg" />
           </a>
           <a href="https://www.smartfrog.com/en-us/" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/smartfrog.png" alt="Smartfrog App Icon" className="w-12 h-12 rounded-lg" />
+            <Image src="/images/smartfrog.png" alt="Smartfrog App Icon" width={48} height={48} className="w-12 h-12 rounded-lg" />
           </a>
         </div>
 
@@ -54,27 +57,27 @@ const Portfolio: React.FC = () => {
         <div className="flex justify-center items-center space-x-4 mb-10">
           {/* Balooga App */}
           <a href="https://apps.apple.com/us/app/balooga/id1450734153" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/balooga.png" alt="Balooga App Icon" className="w-12 h-12 rounded-lg" />
+            <Image src="/images/balooga.png" alt="Balooga App Icon" width={48} height={48} className="w-12 h-12 rounded-lg" />
           </a>
 
           {/* Destinations App */}
           <a href="https://apps.apple.com/us/app/train-destinations-arrive/id1200221308" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/destinations.png" alt="Destinations App Icon" className="w-12 h-12 rounded-lg" />
+            <Image src="/images/destinations.png" alt="Destinations App Icon" width={48} height={48} className="w-12 h-12 rounded-lg" />
           </a>
 
           {/* Compass App */}
           <a href="https://apps.apple.com/sg/app/compass/id926142838" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/compass.png" alt="Compass App Icon" className="w-12 h-12 rounded-lg" />
+            <Image src="/images/compass.png" alt="Compass App Icon" width={48} height={48} className="w-12 h-12 rounded-lg" />
           </a>
 
           {/* Speedometer App */}
           <a href="https://apps.apple.com/sg/app/speedy-speedometer/id991563384" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/speedometer.png" alt="Speedometer App Icon" className="w-12 h-12 rounded-lg" />
+            <Image src="/images/speedometer.png" alt="Speedometer App Icon" width={48} height={48} className="w-12 h-12 rounded-lg" />
           </a>
 
           {/* StoryChef App */}
           <a href="https://apps.apple.com/sg/app/storychef/id6476927109" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/storychef.png" alt="StoryChef App Icon" className="w-12 h-12 rounded-lg" />
+            <Image src="/images/storychef.png" alt="StoryChef App Icon" width={48} height={48} className="w-12 h-12 rounded-lg" />
           </a>
         </div>
 
@@ -85,27 +88,27 @@ const Portfolio: React.FC = () => {
         <div className="flex justify-center space-x-4">
           {/* App Store */}
           <a href="https://apps.apple.com/sg/developer/kleincodes-llc/id717279048" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/appstore.png" alt="AppStore Icon" className="w-9 h-9 rounded-lg" />
+            <Image src="/images/appstore.png" alt="AppStore Icon" width={36} height={36} className="w-9 h-9 rounded-lg" />
           </a>
 
           {/* GitHub */}
           <a href="https://github.com/KleinCodes" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/github.png" alt="Github Icon" className="w-9 h-9 rounded-lg" />
+            <Image src="/images/github.png" alt="Github Icon" width={36} height={36} className="w-9 h-9 rounded-lg" />
           </a>
 
           {/* Twitter */}
           <a href="https://twitter.com/kleincodes" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/twitter.png" alt="Twitter Icon" className="w-9 h-9 rounded-lg" />
+            <Image src="/images/twitter.png" alt="Twitter Icon" width={36} height={36} className="w-9 h-9 rounded-lg" />
           </a>
 
           {/* Instagram */}
           <a href="https://www.instagram.com/kleincodes/?hl=en" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/instagram.png" alt="Instagram Icon" className="w-9 h-9 rounded-lg" />
+            <Image src="/images/instagram.png" alt="Instagram Icon" width={36} height={36} className="w-9 h-9 rounded-lg" />
           </a>
 
           {/* LinkedIn */}
           <a href="https://www.linkedin.com/in/kleincodes" target="_blank" rel="noopener noreferrer" className="transition-transform hover:scale-110">
-            <img src="/images/linkedin.png" alt="LinkedIn Icon" className="w-9 h-9" />
+            <Image src="/images/linkedin.png" alt="LinkedIn Icon" width={36} height={36} className="w-9 h-9" />
           </a>
         </div>
       </div>
