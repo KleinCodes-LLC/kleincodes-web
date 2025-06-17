3c55c0ae9dc38f02f3de1059aabcec56e78636bd
Michael Klein <kleincodes@gmail.com>
2025-06-16 21:02:33 -0400

    Added landing pages.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,58 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/destinations',
+        destination: '/destinations/index.html'
+      },
+      {
+        source: '/destinations/',
+        destination: '/destinations/index.html'
+      },
+      {
+        source: '/destinations/privacy',
+        destination: '/destinations/privacy.html'
+      },
+      {
+        source: '/destinations/privacy/',
+        destination: '/destinations/privacy.html'
+      },
+      {
+        source: '/destinations/terms',
+        destination: '/destinations/terms.html'
+      },
+      {
+        source: '/destinations/terms/',
+        destination: '/destinations/terms.html'
+      },
+      {
+        source: '/storychef',
+        destination: '/storychef/index.html'
+      },
+      {
+        source: '/storychef/',
+        destination: '/storychef/index.html'
+      },
+      {
+        source: '/storychef/privacy',
+        destination: '/storychef/privacy.html'
+      },
+      {
+        source: '/storychef/privacy/',
+        destination: '/storychef/privacy.html'
+      },
+      {
+        source: '/storychef/terms',
+        destination: '/storychef/terms.html'
+      },
+      {
+        source: '/storychef/terms/',
+        destination: '/storychef/terms.html'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
